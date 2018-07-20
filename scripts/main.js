c1ef2a7d93c842df7c042f452f352aaed7002f19
Jay Silvas <jayinlava@gmail.com>
2018-07-20 01:17:54 -0700

    End of night commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 console.log('Signal check!');
+
+// import { get } from 'axios';
+require('axios');
+
+// *********** HTTP Requests ***********
+
+const testUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+// const artifactoryStorageUrl = 'http://35.226.93.161:8093/artifactory/api/storageinfo';
+// const jarStatsUrl = 'http://35.226.93.161:8093/artifactory/api/storage/jcenter-cache/org/apache/tiles/tiles-api/2.0.6/tiles-api-2.0.6.jar?stats';
+
+// const params = {
+// };
+
+axios.get(testUrl)  //Add second input 'params' for queries
+  .then( data => console.log(data))
+  .catch( err => console.error(err));
+
+// axios.get(artifactoryStorageUrl)
+//   .then( data =>  console.log('storage data: ', data))
+//   .catch( err => console.error('storage data: ', err));
